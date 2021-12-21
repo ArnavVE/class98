6c70fbff541aa6ea5c0161996a1fbbcb413c11c2
ArnavVE <96113311+ArnavVE@users.noreply.github.com>
2021-12-21 14:37:51 +0530

    Add files via upload

---
+++ class98/code.py
@@ -0,0 +1,9 @@
+def countwords():
+    filename=input("Enter the file name :")
+    numberofwords = 0
+    file=open(filename,"r")
+    for line in file:
+        words=line.split()
+        numberofwords=numberofwords+len(words)
+    print(numberofwords)
+countwords()
